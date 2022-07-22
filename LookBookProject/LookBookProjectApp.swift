8d15f8d7c704c0ce531982537605f869fcbbcf0c
FARABI DHARMA <74946238+farabidharma88@users.noreply.github.com>
2022-07-22 10:16:57 +0700

    Add files via upload

---
+++ LookBookProject/LookBookProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookBookProjectApp.swift
+//  LookBookProject
+//
+//  Created by Farabi Dharma on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LookBookProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
